47d955ac5ce61fa52aa8e9cb8864565a0bbbbc7f
spencerking <spencerking@users.noreply.github.com>
2020-04-16 23:51:16 -0500

    prepare fcn8s for colab

--- ptsemseg/loader/pascal_voc_loader.py
+++ ptsemseg/loader/pascal_voc_loader.py
@@ -44,7 +44,7 @@ class pascalVOCLoader(data.Dataset):
     def __init__(
         self,
         root,
-        sbd_path=None,
+        sbd_path="/content/benchmark_RELEASE/",
         split="train_aug",
         is_transform=False,
         img_size=512,
